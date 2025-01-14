4120a263ee459740d1d4e733b46b40ce1cb4649e
Jeshan1 <jectionjection@gmail.com>
2025-01-14 16:02:06 +0545

    authentication and middleware explore with routes,protected routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'ensure' => \App\Http\Middleware\EnsureAge::class,
+            'auth' => \App\Http\Middleware\AuthCheck::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
