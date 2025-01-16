6be3f49322b901653bcdd59c0ddc1e8f89ecb315
Jeshan1 <jectionjection@gmail.com>
2025-01-16 09:58:53 +0545

    anonymous component test

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>This is about page</h1>
-</body>
-</html>
+@extends('layouts.master')
+
+@section('content')
+    <div class="mx-36 my-5">
+        <h1 class="p-4 text-3xl font-bold text-blue-600">This is about page.</h1>
+        <x-button class="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700">Submit</x-button>
+    </div>
+@endsection
