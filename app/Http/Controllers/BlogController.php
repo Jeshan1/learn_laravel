c792a974d3226966beb0c858a3dd0ec4bcb8de35
Jeshan1 <jectionjection@gmail.com>
2025-01-17 11:04:48 +0545

    day4 task complete

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\BlogRequest;
+use App\Models\Blog;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
@@ -11,7 +13,8 @@ class BlogController extends Controller
      */
     public function index()
     {
-        return view ('blog');
+        $blogs = Blog::all();
+        return view ('blog', compact('blogs'));
     }
 
     /**
@@ -19,15 +22,23 @@ class BlogController extends Controller
      */
     public function create()
     {
-        //
+        return view('addblog');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(BlogRequest $request)
     {
-        //
+        $data = $request->all();
+        if ($data) {
+            Blog::create($data);
+            return redirect()->route('blogs.index');
+        }
+        else{
+            return redirect()->route('blogs.create');
+        }
+        
     }
 
     /**
